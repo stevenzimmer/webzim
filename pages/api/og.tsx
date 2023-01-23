1f73597a3a6f34ce5bd5bb3a5922d5982fbd5d51
Steven Zimmer <webdevzim@gmail.com>
2023-01-23 15:51:28 -0800

    open graph update

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -14,43 +14,52 @@ export default async function handler(req: NextRequest) {
   const [sfProData] = await Promise.all([sfPro]);
 
   const { searchParams } = req.nextUrl;
-  const title = searchParams.get("title") || "Web Zim";
+  const title = searchParams.get("title") || "Steven Zimmer";
 
   return new ImageResponse(
     (
       <div
-        style={{
-          height: "100%",
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "white",
-          backgroundImage:
-            "linear-gradient(to bottom right, #E0E7FF 25%, #ffffff 50%, #CFFAFE 75%)",
-        }}
-      >
-        {/* <img
-          src={new URL("../../public/logo.png", import.meta.url).toString()}
-          alt="Precedent Logo"
-          tw="w-20 h-20 mb-4 opacity-95"
-        /> */}
-        <h1
-          style={{
-            fontSize: "100px",
-            fontFamily: "SF Pro",
-            background:
-              "linear-gradient(to bottom right, #000000 21.66%, #78716c 86.47%)",
-            backgroundClip: "text",
-            color: "transparent",
-            lineHeight: "5rem",
-            letterSpacing: "-0.02em",
-          }}
-        >
-          {title}
-        </h1>
-      </div>
+  style={{
+    height: "100%",
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+    backgroundImage:
+      "linear-gradient(to bottom right, #eff6ff 25%, #ffffff 50%, #dbeafe 75%)",
+  }}
+>
+
+    <h1
+      style={{
+        fontSize: "80px",
+        fontFamily: "SF Pro",
+        background:
+          "linear-gradient(to bottom right, #000000 21.66%, #78716c 86.47%)",
+        backgroundClip: "text",
+        color: "transparent",
+        lineHeight: "5rem",
+      }}
+    >
+      Steven Zimmer
+    </h1>
+    
+    <h2 style={{
+      fontSize: "30px",
+      fontFamily: "SF Pro",
+      background:
+        "linear-gradient(to bottom right, #000000 21.66%, #78716c 86.47%)",
+      backgroundClip: "text",
+      color: "transparent",
+      lineHeight: "3rem",
+    }}>
+      Full Stack | Web Strategy | Growth Marketing
+    </h2>
+  
+</div>
+
     ),
     {
       width: 1200,
