6e9b031808983e66d50d8fca6cebaa29f3a2d29c
Steven Zimmer <webdevzim@gmail.com>
2023-12-09 14:12:32 -0800

    disable button

--- components/openai-dall-e/form.tsx
+++ components/openai-dall-e/form.tsx
@@ -39,7 +39,7 @@ export default function Form() {
   <>
     <form className="our-form my-12" onSubmit={handleSubmit}>
           <input placeholder="Describe the image you want to generate..." className="w-full border-slate-200 rounded mb-2" type="text" onChange={handleChange} />
-          <button className=" bg-indigo-200 rounded px-3 py-2">Generate image</button>
+          <button disabled={isLoading} className=" bg-indigo-200 rounded px-3 py-2 disabled:bg-slate-200 disabled:cursor-not-allowed disabled:text-slate-400">Generate image</button>
         </form>
         <div className="h-screen">
         {errorMsg && (
@@ -50,7 +50,7 @@ export default function Form() {
 
         {isLoading && (
           <div>
-            <svg className="animate-spin h-10 w-10 text-slate-800 mx-auto mb-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <svg className="animate-spin h-10 w-10 text-slate-200 mx-auto mb-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
               <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
               <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
             </svg>
