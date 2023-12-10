a5e23475b541faedce4ee8625313386f7b8f2b12
Steven Zimmer <webdevzim@gmail.com>
2023-12-10 06:52:37 -0800

    remove dalle

--- app/page.tsx
+++ app/page.tsx
@@ -407,19 +407,20 @@ const projects: DictItem[] = [
   //   large:false
   
   // },
-  {
-  title: "Tic Tac Toe",
-  bullets: [ "A showcase of React, State Management, and Tailwind CSS."],
+//   {
+//   title: "Tic Tac Toe",
+//   bullets: [ "A showcase of React, State Management, and Tailwind CSS."],
   
-  href: "/tic-tac-toe",
-  demo: (
+//   href: "/tic-tac-toe",
+//   demo: (
 
-    <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 256 256" className="w-16 text-slate-600"  xmlns="http://www.w3.org/2000/svg"><path d="M216,148H172V108h44a12,12,0,0,0,0-24H172V40a12,12,0,0,0-24,0V84H108V40a12,12,0,0,0-24,0V84H40a12,12,0,0,0,0,24H84v40H40a12,12,0,0,0,0,24H84v44a12,12,0,0,0,24,0V172h40v44a12,12,0,0,0,24,0V172h44a12,12,0,0,0,0-24Zm-108,0V108h40v40Z"></path></svg>
+//     <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 256 256" className="w-16 text-slate-600"  xmlns="http://www.w3.org/2000/svg"><path d="M216,148H172V108h44a12,12,0,0,0,0-24H172V40a12,12,0,0,0-24,0V84H108V40a12,12,0,0,0-24,0V84H40a12,12,0,0,0,0,24H84v40H40a12,12,0,0,0,0,24H84v44a12,12,0,0,0,24,0V172h40v44a12,12,0,0,0,24,0V172h44a12,12,0,0,0,0-24Zm-108,0V108h40v40Z"></path></svg>
      
-  ),
-  large:false
+//   ),
+//   large:false
 
-},{
+// },
+{
     title: "Mdrn Dsgn",
     bullets: [ "A showcase of modern design trends on a fake company website."],
     
